1db7e5baeceebe10766e2be7a585e2b3d3865884
maxca17 <maxcalvert34@gmail.com>
2023-06-21 16:53:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [file1, setFile1] = useState(null);
+  const [file2, setFile2] = useState(null);
+
+  const onFileChange1 = (event) => {
+    setFile1(event.target.files[0]);
+  };
+
+  const onFileChange2 = (event) => {
+    setFile2(event.target.files[0]);
+  };
+
+  const onFilesUpload = async () => {
+    const formData = new FormData();
+
+    // append the files to formData
+    formData.append("file1", file1);
+    formData.append("file2", file2);
+
+    console.log(file1, file2);
+
+    // Make a post request to the API endpoint
+    const response = await axios.post("http://localhost:8000/upload", formData);
+    console.log(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>File Upload</h1>
+      <div className="format-image">
+      <img src="path/to/your/image.jpg" alt="Resume Format" />
+      </div>
+        <h2>Upload your current resume information in a word document in the format provided below</h2>
+        <div className='button'>
+        <input type="file" onChange={onFileChange1} />
+      </div>
+      <div>
+        <input className='button' type="file" onChange={onFileChange2} />
+      </div>
+      <button onClick={onFilesUpload}>Upload Files!</button>
     </div>
   );
-}
+};
 
 export default App;
